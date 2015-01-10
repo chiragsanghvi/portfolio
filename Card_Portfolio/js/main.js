643300ae64ed31aa16d6706693036172d5eb1b97
chiragsanghvi <csanghvi@appacitive.com>
2015-01-10 13:59:24 +0530

    added new portfolio

---
+++ Card_Portfolio/js/main.js
@@ -0,0 +1,411 @@
+jQuery(document).ready(function($) {
+
+    /* ---------------------------------------------------------------------- */
+    /*	------------------------------- Loading ----------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    /*Page Preloading*/
+    $(window).load(function() {
+        $('#spinner').fadeOut(200);
+        $('#preloader').delay(200).fadeOut('slow');
+        $('.wrapper').fadeIn(200);
+        $('#custumize-style').fadeIn(200);
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------------- Taps profile ------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $('.collapse_tabs').click(function() {
+
+        if ($(this).hasClass('collapsed')) {
+            $(this).find('i.glyphicon').removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+        } else {
+            $(this).find('i.glyphicon').removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+        }
+
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------- easyResponsiveTabs ------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    $('#verticalTab').easyResponsiveTabs({
+        type: 'vertical',
+        width: 'auto',
+        fit: true
+    });
+
+    $("h2.resp-accordion").click(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_active");
+        $("h2.resp-accordion").not(this).find(".icon_menu").removeClass("icon_menu_active");
+
+        /*	Scroll To */
+        $('html, body').animate({scrollTop: $('h2.resp-accordion').offset().top - 50}, 600);
+    });
+
+    $(".resp-tabs-list li").click(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_active");
+        $(".resp-tabs-list li").not(this).find(".icon_menu").removeClass("icon_menu_active");
+    });
+
+
+    $(".resp-tabs-list li").hover(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_hover");
+    }, function() {
+        $(this).find(".icon_menu").removeClass("icon_menu_hover");
+    });
+
+    $("h2.resp-accordion").hover(function() {
+        $(this).find(".icon_menu").addClass("icon_menu_hover");
+    }, function() {
+        $(this).find(".icon_menu").removeClass("icon_menu_hover");
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* --------------------------- Scroll tabs ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    $(".content_2").mCustomScrollbar({
+        theme: "dark-2",
+        contentTouchScroll: true,
+        advanced: {
+            updateOnContentResize: true,
+            updateOnBrowserResize: true,
+            autoScrollOnFocus: false
+        }
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------- Effect tabs -------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    var animation_style = 'bounceIn';
+
+    $('.dropdown-select').change(function() {
+        animation_style = $('.dropdown-select').val();
+    });
+
+
+    $('ul.resp-tabs-list li[class^=tabs-]').click(function() {
+
+        var tab_name = $(this).attr('data-tab-name');
+
+        $('.resp-tabs-container').addClass('animated ' + animation_style);
+        $('.resp-tabs-container').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+            $('.resp-tabs-container').removeClass('animated ' + animation_style);
+        });
+
+        $(".content_2").mCustomScrollbar("destroy");
+        $(".content_2").mCustomScrollbar({
+            theme: "dark-2",
+            contentTouchScroll: true,
+            advanced: {
+                updateOnContentResize: true,
+                updateOnBrowserResize: true,
+                autoScrollOnFocus: false
+            }
+        });
+
+        if (tab_name == "contact")
+            initialize();
+
+        return false;
+    });
+
+    $("#verticalTab h2.resp-accordion").click(function() {
+        initialize();
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------- redimensionnement ----------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    function redimensionnement() {
+
+        if (window.matchMedia("(max-width: 800px)").matches) {
+            $(".content_2").mCustomScrollbar("destroy");
+            $(".resp-vtabs .resp-tabs-container").css("height", "100%");
+            $(".content_2").css("height", "100%");
+        } else {
+
+            $(".resp-vtabs .resp-tabs-container").css("height", "580px");
+            $(".content_2").css("height", "580px");
+            $(".content_2").mCustomScrollbar("destroy");
+            $(".content_2").mCustomScrollbar({
+                theme: "dark-2",
+                contentTouchScroll: true,
+                advanced: {
+                    updateOnContentResize: true,
+                    updateOnBrowserResize: true,
+                    autoScrollOnFocus: false
+                }
+            });
+
+        }
+
+    }
+
+    // On lie l'événement resize à la fonction
+    window.addEventListener('load', redimensionnement, false);
+    window.addEventListener('resize', redimensionnement, false);
+
+    $("#verticalTab h2.resp-accordion").click(function() {
+        initialize();
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------- Contact Form ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+    // Needed variables
+    var $contactform = $('#contactform'),
+            $success = ' Your message has been sent. Thank you!';
+
+    Appacitive.initialize({ 
+      apikey: "OIprEphvKPFdT+QUO3tLn30tVo3uRorNkfrqKwIKOP8= ",// The master or client api key for your app on appacitive.
+      env: "sandbox",      // The environment that you are targetting (sandbox or live).
+      appId: "79397178698761175"     // The app id for your app on appacitive. 
+    });
+
+
+    $contactform.submit(function() {
+
+        var e, t = [];
+        if ($("#contact-name input").val().trim().length == 0) {
+            $("#contact-name").addClass("has-error");
+            $("#contact-name").removeClass("has-success");
+            t.push("Your Name");
+        } else {
+            $("#contact-name").addClass("has-success");
+            $("#contact-name").removeClass("has-error");
+        }
+
+        if ($("#contact-email input").val().trim().length == 0) {
+            $("#contact-email").addClass("has-error");
+            $("#contact-email").removeClass("has-success");
+            t.push("Your email");
+        } else {
+            if(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test($("#contact-email input").val())) {
+                $("#contact-email").addClass("has-success");
+                $("#contact-email").removeClass("has-error");
+            } else {
+                $("#contact-email").addClass("has-error");
+                $("#contact-email").removeClass("has-success");
+                t.push('Invalid Email');
+            }
+        }
+
+        if ($("#contact-message textarea").val().trim().length == 0) {
+            $("#contact-message").addClass("has-error");
+            $("#contact-message").removeClass("has-success");
+            t.push("Message");
+        } else {
+            $("#contact-message").addClass("has-success");
+            $("#contact-message").removeClass("has-error");
+        }
+
+        if (t.length > 0) {
+                var error = '<div class="alert alert-danger error-send">' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                            '<i class="glyphicon glyphicon-remove" style="margin-right: 5px;"></i> Required:' + t.toString()
+                            + '</div>';
+
+                
+                // Hide any previous response text
+                $(".error-send,.success-send").remove();
+                // Show response message
+                $contactform.prepend(error);
+        } else {
+
+            Appacitive.Email.sendRawEmail({
+                to: ["chirag_sanghvi7@hotmail.com"],
+                from: [$("#contact-email input").val()],
+                replyto:$("#contact-email input").val(),
+                subject: "Portfolio Site Message from " + $("#contact-name").val(),
+                body: $("#contact-message textarea").val(),
+                ishtml: true
+            }).then(function (email) {
+                response = '<div class="alert alert-success success-send">' +
+                            '<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+                            '<i class="glyphicon glyphicon-ok" style="margin-right: 5px;"></i>Email sent successfully</div>';
+
+                $(".reset").trigger('click');
+                $("#contact-name").removeClass("has-success");
+                $("#contact-email").removeClass("has-success");
+                $("#contact-message").removeClass("has-success");
+                $(".error-send,.success-send").remove();
+                // Show response message
+                $contactform.prepend(response);
+            });
+
+        }
+
+        return false;
+    });
+
+    /* ---------------------------------------------------------------------- */
+    /* ----------------------------- Portfolio ------------------------------ */
+    /* ---------------------------------------------------------------------- */
+
+
+    var filterList = {
+        init: function() {
+
+            // MixItUp plugin
+            // http://mixitup.io
+            $('#portfoliolist').mixitup({
+                targetSelector: '.portfolio',
+                filterSelector: '.filter',
+                effects: ['fade'],
+                easing: 'snap',
+                // call the hover effect
+                onMixEnd: filterList.hoverEffect()
+            });
+
+        },
+        hoverEffect: function() {
+
+            // Simple parallax effect
+            $('#portfoliolist .portfolio').hover(
+                    function() {
+                        $(this).find('.label').stop().animate({bottom: 0}, 200);
+                        //$(this).find('img').stop().animate({top: -30}, 500);
+                    },
+                    function() {
+                        $(this).find('.label').stop().animate({bottom: -40}, 200);
+                        //$(this).find('img').stop().animate({top: 0}, 300);
+                    }
+            );
+
+        }
+
+    };
+
+    // Run the show!
+    filterList.init();
+
+    /* ---------------------------------------------------------------------- */
+    /* ----------------------------- prettyPhoto ---------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $("a[rel^='portfolio']").prettyPhoto({
+        animation_speed: 'fast', /* fast/slow/normal */
+        social_tools: '',
+        theme: 'pp_default',
+        horizontal_padding: 5,
+        deeplinking: false,
+        inline_markup: '<div class="pp_inline" id="home">{content}</div>',
+        changepicturecallback: function(){
+            if($('.pp_inline').length > 0) {
+                $('.pp_inline').parents('.pp_content')
+                    .css('height', $('.pp_inline').height() + 50);
+            }
+        }
+    });
+
+
+
+    /* ---------------------------------------------------------------------- */
+    /* ------------------------------ Google Maps --------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    var map;
+    function initialize() {
+        map = new GMaps({
+            div: '#map',
+            lat: 18.5395208,
+            lng: 73.8953392,
+            zoom: 14
+
+        });
+        map.addMarker({
+            lat: 18.5395208,
+            lng: 73.8953392,
+            title: '',
+            icon: 'images/map-pins/marker.png',
+            infoWindow: {
+                content: '<p>Pune, Maharshtra India</p>'
+            }
+        });
+    }
+
+    /* ---------------------------------------------------------------------- */
+    /* ---------------------------- icon menu ------------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $(".resp-tabs-container h2.resp-accordion").each(function(){
+			 
+			if($(this).hasClass('resp-tab-active')){
+				$(this).append("<i class='glyphicon glyphicon-chevron-up arrow-tabs'></i>");
+			}else {
+				$(this).append("<i class='glyphicon glyphicon-chevron-down arrow-tabs'></i>");
+			}
+	  });
+	  
+	   $(".resp-tabs-container h2.resp-accordion").click(function(){
+			if($(this).hasClass('resp-tab-active')){
+				$(this).find("i.arrow-tabs").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up");
+			}
+			
+			$(".resp-tabs-container h2.resp-accordion").each(function(){
+		 
+				if(!$(this).hasClass('resp-tab-active')){
+					$(this).find("i.arrow-tabs").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down");
+				}
+		  });
+	  
+			
+	  });
+
+
+    /* ---------------------------------------------------------------------- */
+    /* -------------------------------- skillbar ---------------------------- */
+    /* ---------------------------------------------------------------------- */
+
+    $('.tabs-resume').click(function() {
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
+
+    });
+
+    $('#resume').prev('h2.resp-accordion').click(function() {
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').width(0);
+        });
+
+        $('.skillbar').each(function() {
+            $(this).find('.skillbar-bar').animate({
+                width: $(this).attr('data-percent')
+            }, 2000);
+        });
+    });
+	
+		
+	//Change for demo page
+    $('input:radio[name=page_builder]').on('change', function() {
+		
+		$('input:radio[name=page_builder]').each(function () {
+
+			var $this = $(this);
+	
+			if ($(this).prop('checked')) {
+				window.location.replace($this.val());
+			}
+		});
+		
+        return false;
+    });
+
+
+
+});
